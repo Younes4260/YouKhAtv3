b06746cf0e4df2ab9ec57c37e725d2f8c45bb0bc
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2022-02-23 17:25:50 -0500

    Updates

---
+++ precache-manifest.1cc5de1106317f220a8aee9b7307558b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ec8e95b77da526181d39297785f69d",
+    "url": "/YouKhAtv3/index.html"
+  },
+  {
+    "revision": "973d48bfb60c279d123f",
+    "url": "/YouKhAtv3/static/css/main.d5c0ca3d.chunk.css"
+  },
+  {
+    "revision": "a152c03e1f0c36557b5c",
+    "url": "/YouKhAtv3/static/js/2.188bb9a2.chunk.js"
+  },
+  {
+    "revision": "df846bc52d934438724c241c3efb95c9",
+    "url": "/YouKhAtv3/static/js/2.188bb9a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "973d48bfb60c279d123f",
+    "url": "/YouKhAtv3/static/js/main.1cde082e.chunk.js"
+  },
+  {
+    "revision": "cdcef65542c4ce0857a2",
+    "url": "/YouKhAtv3/static/js/runtime-main.09a0a7ad.js"
+  }
+]);
