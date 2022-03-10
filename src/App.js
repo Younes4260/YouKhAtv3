182847cb0cc9109332ec923f3d86d22c2321cfac
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2022-03-09 20:49:29 -0500

    Finalisation pour la presentation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* Importation des fichiers nécessaires */
+
 import React, { useRef, useState } from 'react';
 import './App.css';
 
@@ -10,6 +12,8 @@ import ReactTooltip from "react-tooltip";
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+/* Connection du firebase au projet */
 
 firebase.initializeApp({
   apiKey: "AIzaSyCc56Iksic3DyqOSsKhF-8l1EXRh-cF8SY",
@@ -26,7 +30,7 @@ const firestore = firebase.firestore();
 const analytics = firebase.analytics();
 
 function App() {
- const OnPrototype = false;
+
   return (
     <div className="App">
       <Site />
@@ -36,24 +40,29 @@ function App() {
 }
 
 class Site extends React.Component {
-  
+/* Cette fonction affiche le UI du Prototype ou le UI de la Présentation
+Cela depend de l'interaction */
   constructor(props){
     super(props)
     this.state = {
+      /* L'état de base est faux */
      isOnPrototype: false
     }
  
+    /* Nécessaire pour permette de faire des interactions avec des class */
     this.toggleState = this.toggleState.bind(this);
   }
     
   toggleState() {
   this.setState ({
+    /* Change le status pour l'inverse de ce qu'il est présentement */
    isOnPrototype:!this.state.isOnPrototype }
   )
  } 
 
   render(){
    if(this.state.isOnPrototype){
+     /* Si isOnPrototype est true, alors on affiche la fonction prototype, qui affiche le prototype */
      return(
        <>
        <div id="BoutonToggle">
@@ -65,6 +74,7 @@ class Site extends React.Component {
      )
      }
    else{
+     /* Si isOnPrototype est false, alors on affiche la fonction présentation, qui affiche la présentation */
      return(
      <>
      <div id="BulleToggle">
@@ -78,7 +88,7 @@ class Site extends React.Component {
  }
 }
 
-
+/* Notre générateur de bulles */
 function Bulles() {
   return (
     <div id="background-wrap">
@@ -100,7 +110,9 @@ class Presentation extends React.Component {
   constructor(props){
     super(props)
     this.state = {
+      /* Ici, c'est le même concept qu'avec le changement entre la présentation et le prototype, mais cette fois-ci avec des lightbox */
      isOnLightbox: false,
+     /* Ce numéro est important afin de traquer dans quelle div l'utilisateur a cliqué. Le contenu généré va ainsi dépendre */
      NumberText: 0
     }
  
@@ -109,19 +121,24 @@ class Presentation extends React.Component {
     
   toggleState() {
   this.setState ({
+     /* Change le status pour l'inverse de ce qu'il est présentement */
    isOnLightbox:!this.state.isOnLightbox 
   })
  } 
 
  render() {
-  const texte = ["Ce truc a été fait avec React et c'est très cool", 
-  "Firebase est trop fort utilisez-le!", 
-  "CodeAcademy est nice aussi allez acheter!"]
-  const titre = ["Avec React.js", "Avec Firebase", "Sur Codecademy"]
+    /* Les textes et les titres sont gérées ici. Changer le texte et le contenu changera aussi */
+  const texte = ["React.js est une librairie Javascript qui offre beaucoup de possibilités afin de créer multiple interactions du côté client. On peut afficher du contenu dans certaines conditions aussi.", 
+  "Firebase est une base de données gratuite et en ligne. Il est très facile de connecter Firebase avec React.js et de créer du contenu qui est géré en back-end.", 
+  "Codecademy est un site web qui offre des cours d'apprentissage autonome, gratuit et avec instructions sur des langages de programmation, telles que le Javascript et React.Js. C'est comme ça que j'ai ainsi appris à pouvoir lire et écrire du React."]
+  const titre = ["Avec React.js", "Avec Firebase", "Appris sur Codecademy"]
+
+  /* Sélectionne le texte à afficher, basé sur le NumberText d'en haut */
   const textelire = texte[this.state.NumberText]
   const titrelire = titre[this.state.NumberText]
 
   if(this.state.isOnLightbox) {
+    /* Si isOnLightbox est true, on affiche le contenu avec lightbox */
   return (
   <div className="Intro">
     <div className="backgroundlightbox">
@@ -137,13 +154,13 @@ class Presentation extends React.Component {
     </div>
 <section>
     <div id = "bloc1" className="bullesection">
-        <h1>Avec React.js</h1>
+        <h1>{titre[0]}</h1>
     </div>
     <div id = "bloc2" className="bullesection">
-        <h1>Avec une base de données</h1>
+        <h1>{titre[1]}</h1>
     </div>
     <div id = "bloc3" className="bullesection">
-        <h1>Appris sur codeacadamy</h1>
+        <h1>{titre[2]}</h1>
     </div>
 </section>
 </div>
@@ -155,26 +172,28 @@ class Presentation extends React.Component {
   <section>
       <ReactTooltip globalEventOff="click" />
       <div id = "bloc1" className="bullesection" data-tip="Cliquez pour plus de détail!" onClick={() => {
+        /* Cliquer sur une bulle spécifique de la section va changer cette variable et ainsi générer le contenu
+        basé sur ce qui a été cliqué */
         this.toggleState();
         this.setState ({
           NumberText: 0
         })}}
         >
-          <h1>Avec React.js</h1>
+          <h1>{titre[0]}</h1>
       </div>
       <div id = "bloc2" className="bullesection" data-tip="Cliquez pour plus de détail!" onClick={() => {
         this.toggleState();
         this.setState ({
           NumberText: 1
         })}} >
-          <h1>Avec une base de données</h1>
+          <h1>{titre[1]}</h1>
       </div>
       <div id = "bloc3" className="bullesection" data-tip="Cliquez pour plus de détail!" onClick={() => {
         this.toggleState();
         this.setState ({
           NumberText: 2
         })}}>
-          <h1>Appris sur codeacadamy</h1>
+          <h1>{titre[2]}</h1>
       </div>
   </section>
   </div>
@@ -187,6 +206,7 @@ class Presentation extends React.Component {
 
 function Prototype() {
   const [user] = useAuthState(auth);
+    /* Si on est connecté, on affiche le ChatRoom, sinon on affiche le SignIn */
  return (
   <div className="Prototype">
   <header>
@@ -203,6 +223,7 @@ function Prototype() {
 function SignIn() {
 
   const signInWithGoogle = () => {
+    /* Permet de se connecter avec google sur notre firebase */
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
@@ -219,6 +240,7 @@ function SignIn() {
 }
 
 function SignOut() {
+  /* Permet de déconnecter du compte google */
   return auth.currentUser && (
     <>
      <ReactTooltip globalEventOff="click" />
@@ -232,6 +254,8 @@ function SignOut() {
 
 function ChatRoom() {
   const dummy = useRef();
+
+   /* On prends les messages de notre collection et on les limite aux 50 derniers, basé sur leur temps d'envoi */
   const messagesRef = firestore.collection('messages');
   const query = messagesRef.orderBy('createdAt').limitToLast(50);
   const [messages] = useCollectionData(query, { idField: 'id' });
@@ -243,7 +267,7 @@ function ChatRoom() {
     e.preventDefault();
 
     const { uid, photoURL } = auth.currentUser;
-
+/* On cherche le texte du form, le temps d'envoi, l'uid et l'url de la photo google de celui qui envoie un */
     await messagesRef.add({
       text: formValue,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
@@ -256,6 +280,8 @@ function ChatRoom() {
   }
 
   return (
+    /* C'est ici que tous les messages se retournent en forme de array */
+    /* La fonction qui envoie des messages s'activent quand on appuie sur le bouton */
     <section>
     <main>
 
@@ -276,15 +302,17 @@ function ChatRoom() {
   )
 }
 
-
+/* L'array se transforme en blocs de div séparés et ainsi sont lus*/
 function ChatMessage(props) {
   const { text, uid, photoURL} = props.message;
 
+  /* Si c'est nous qui envoyons un message, on ajoute la classe sent, sinon, on ajoute la classe received */
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   // const DeleteMessage = firestore.collection('messages').doc('RAoTYP5A99uEfvmYc0dp').delete()
 
-  return (<>
+  return (</* Le contenu est finalement lu dans le site */>
+  
     <div className={`message ${messageClass}`}>
       <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
       <p>{text}</p>
